24a91b423591f4d6ff893b186c4ff089ade39e95
Rene Carballo <rcarballo2222@gmail.com>
2018-10-04 19:47:07 -0500

    Implemented Segment formatter module

--- Project 1/lab1.v
+++ Project 1/lab1.v
@@ -64,11 +64,29 @@ endmodule // calculator
  */
 module segmentFormatter(
 	input [3:0] num_pi,
-	output reg [6:0] disp_po
+	output reg [6:0] disp_po		 
 );
 	
 // STEP 1: Implement the segmentFormatter module
-
+always @(*) begin
+		case(num_pi)                //6543210
+		4'b0000:	disp_po[7:0] = 7'b0111111; //0
+		4'b0001:	disp_po[7:0] = 7'b0000110; //1
+		4'b0010:	disp_po[7:0] = 7'b1011011; //2
+		4'b0001:	disp_po[7:0] = 7'b1001111; //3
+		4'b0001:	disp_po[7:0] = 7'b1100110; //4
+		4'b0001:	disp_po[7:0] = 7'b1101101; //5
+		4'b0001:	disp_po[7:0] = 7'b1111101; //6
+		4'b0001:	disp_po[7:0] = 7'b0000111; //7
+		4'b0001:	disp_po[7:0] = 7'b1111111; //8
+		4'b0001:	disp_po[7:0] = 7'b1100111; //9
+		4'b0001:	disp_po[7:0] = 7'b1110111; //10 - A
+		4'b0001:	disp_po[7:0] = 7'b1111100; //11 - B
+		4'b0001:	disp_po[7:0] = 7'b0111001; //12 - C
+		4'b0001:	disp_po[7:0] = 7'b1011110; //13 - D
+		4'b0001:	disp_po[7:0] = 7'b1111001; //14 - E
+		4'b0001:	disp_po[7:0] = 7'b1110001; //15 - F
+		endcase
 
 // END STEP 1
 endmodule // segmentFormatter
