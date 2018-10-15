b5686abb43b404fca8760fe981ff23a6bd0572ca
FredyJMartinez <fredyjmartinez99@gmail.com>
2018-10-14 22:38:23 -0500

    Update Project 1/lab1.v

--- Project 1/lab1.v
+++ Project 1/lab1.v
@@ -2,48 +2,48 @@
 
 /*
  * Module: calculator
- * Description: The top module of this lab 
+ * Description: The top module of this lab
  */
 module calculator(
-	input CLK, 
-	input [7:0] SW,  
-	input [3:0] BTN, 
+	input CLK,
+	input [7:0] SW,
+	input [3:0] BTN,
 	output [7:0] LED,
 	output [6:0] SEG,
 	output DP,
 	output [3:0] AN
-); 
- 
+);
+
 	reg  [15:0] display_num; //TODO: Change back to reg
 	wire [15:0] counter;
 	wire clk_en;
-	
-        // STEP 2 - implement the calculator logic here 
-        always@(*) begin 
-		
+
+        // STEP 2 - implement the calculator logic here
+        always@(*) begin
+
                 casez(BTN)
         	4'b0000 : display_num = SW;
-        	4'b1000 : display_num = SW[7:4]+SW[3:0];      
+        	4'b1000 : display_num = SW[7:4]+SW[3:0];
         	4'b0100 : display_num = SW[7:4]*SW[3:0];
         	4'b0010 : display_num = SW[7:4]^SW[3:0];
-        	4'b0001 : display_num[1:0] = {SW[3]&~SW[1], SW[2]&~SW[1] + SW[3]&SW[2] + SW[3]&~SW[0]}; 
+        	4'b0001 : display_num[1:0] = {SW[3]&~SW[1], SW[2]&~SW[1] | SW[3]&SW[2] | SW[3]&~SW[0]}; 
         	4'b???? : display_num = counter;
-		endcase	
+		endcase
         end
-        
+
 	// END STEP 2
-	
+
 	assign LED = display_num[7:0];
-	
+
 	clkdiv #(.SIZE(10)) Iclkdiv (
 		.clk_pi(CLK),
 		.clk_en_po(clk_en));
-	
+
 	increment Iincrement (
 		.clk_pi(CLK),
 		.counter_po(counter)
 	);
-	
+
 	sevenSegDisplay IsevenSegDisplay (
 		.clk_pi(CLK),
 		.clk_en_pi(clk_en),
@@ -51,32 +51,32 @@ module calculator(
 		.seg_po(SEG),
 		.dp_po(DP),
 		.an_po(AN));
-		
+
 
 endmodule // calculator
 
 
 
-/* 
+/*
  * Module: segmentFormatter
- * 
+ *
  * Description: Combinational logic for the seven segment bits of a digit of the seven segment display
  *
  * 0 - LSB of disp_po
  * 6 - MSB of disp_po
- *      --(0)   
+ *      --(0)
  * (5)|      |(1)
  *      --(6)
  * (4)|      |(2)
- *      --(3) 
- * 
- * disp_po is active low 
+ *      --(3)
+ *
+ * disp_po is active low
  */
 module segmentFormatter(
 	input [3:0] num_pi,
 	output reg [6:0] disp_po
 );
-	
+
 // STEP 1: Implement the segmentFormatter module
 
 always @(*) begin
@@ -114,25 +114,25 @@ module sevenSegDisplay(
 	output dp_po,
 	output reg [3:0] an_po
 );
-	
+
 	wire [6:0] disp0, disp1, disp2, disp3;
-	
+
 	assign dp_po = 1'b1;
-	
-	
+
+
 	segmentFormatter IsegmentFormat0 ( .num_pi(num_pi[3:0]),   .disp_po(disp0));
 	segmentFormatter IsegmentFormat1 ( .num_pi(num_pi[7:4]),   .disp_po(disp1));
 	segmentFormatter IsegmentFormat2 ( .num_pi(num_pi[11:8]),  .disp_po(disp2));
 	segmentFormatter IsegmentFormat3 ( .num_pi(num_pi[15:12]), .disp_po(disp3));
-	
+
 	initial begin
 		seg_po <= 7'h7F;
 		an_po <= 4'b1111;
 	end
-	
+
 	always @(posedge clk_pi) begin
 		if(clk_en_pi) begin
-			case(an_po) 
+			case(an_po)
 				4'b1110: begin
 					seg_po <= disp1;
 					an_po  <= 4'b1101;
@@ -166,27 +166,27 @@ module clkdiv (
 	input clk_pi,
 	output clk_en_po
 );
-	
+
 	parameter SIZE = 8;
-	
+
 	reg [SIZE-1:0] counter;
-		
+
 	initial begin
 		counter <= 0;
 	end
-	
+
 	always @(posedge clk_pi) begin
 		counter = counter + 1;
 	end
-	
-	assign clk_en_po = (counter == {SIZE{1'b0}}); 
-	
+
+	assign clk_en_po = (counter == {SIZE{1'b0}});
+
 endmodule // clkdiv
 
 
 /*
  * Module: increment
- * Description: Constantly increments a sixteen bit number 
+ * Description: Constantly increments a sixteen bit number
  */
 module increment (
 	input clk_pi,
@@ -195,14 +195,11 @@ module increment (
 	localparam SIZE = 40;
 
 	reg [SIZE:0] count;
-	
+
 	always @(posedge clk_pi) begin
 		count = count+1;
 	end
-	
+
 	assign counter_po = count[SIZE:SIZE-15];
 
 endmodule // increment
-
-	
- 
